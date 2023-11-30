4d28177772795ad26669140cd0b906538acf1f06
Whice <andnotmailru@gmail.com>
2023-11-30 12:27:57 +0300

    feat: настроена система управления.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,61 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Rendering;
+using UnityEditor.Rendering.LookDev;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Mover : MonoBehaviour
+namespace SpaceHope.View.Components
 {
-    public float speed = 2f;
-    public Vector2 rotationVectorSpeed = Vector2.up;
-    public Vector2 rotationVector = Vector2.up;
-    public Vector3 test = Vector3.up;
-
-    private Quaternion rotateQuad;
-    void Start()
+    public class Mover : MonoBehaviour
     {
-        rotateQuad = Quaternion.Euler(test.x,test.y,0);
-    }
+        public float speed = 2f;
+        public Vector2 rotationVectorSpeed = Vector2.up;
+        public Vector2 rotationVector = Vector2.up;
+        public Vector3 test = Vector3.up;
 
-    private void Rotate()
-    {
-        rotationVector = new Vector2(0, 0);
+        private Quaternion rotateQuad;
+        public Vector2 rotateAxis = Vector2.zero;
 
-        if (Input.GetKey(KeyCode.W))
+        private void Rotate()
         {
-            rotationVector = new Vector2(rotationVectorSpeed.x, rotationVector.y);
+            rotationVector = new Vector2(rotateAxis.x * rotationVectorSpeed.x, rotateAxis.y * rotationVectorSpeed.y);
+            if (rotationVector.x != 0 || rotationVector.y != 0)
+            {
+                rotateQuad = Quaternion.Euler(rotationVector.x, rotationVector.y, 0);
+                transform.rotation *= rotateQuad;
+            }
         }
-        else if (Input.GetKey(KeyCode.S))
+        void Update()
         {
-            rotationVector = new Vector2(-rotationVectorSpeed.x, rotationVector.y);
-        }
+            Rotate();
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            rotationVector = new Vector2(rotationVector.x, rotationVectorSpeed.y);
+            Vector3 currentPosition = transform.localPosition;
+            Vector3 direction = transform.forward;
+            float currentSpeed = speed * Time.deltaTime;
+            transform.localPosition = new Vector3
+                (
+                currentPosition.x + direction.x * currentSpeed,
+                currentPosition.y + direction.y * currentSpeed,
+                currentPosition.z + direction.z * currentSpeed
+                );
         }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            rotationVector = new Vector2(rotationVector.x, -rotationVectorSpeed.y);
-        }
-
-        if (rotationVector.x != 0 || rotationVector.y != 0)
-        {
-            rotateQuad = Quaternion.Euler(-rotationVector.x, -rotationVector.y, 0);
-            transform.rotation *= rotateQuad;
-        }
-    }
-    void Update()
-    {
-        Rotate();
-
-        Vector3 currentPosition = transform.localPosition;
-        Vector3 direction = transform.forward;
-        float currentSpeed = speed * Time.deltaTime;
-        transform.localPosition = new Vector3
-            (
-            currentPosition.x + direction.x * currentSpeed,
-            currentPosition.y + direction.y * currentSpeed,
-            currentPosition.z + direction.z * currentSpeed
-            );
     }
 }
---
+++ Assets/Sources/Input/PlayerRotateInput.cs
@@ -0,0 +1,48 @@
+﻿using SpaceHope.View.Components;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace SpaceHope.View.Input
+{
+    [RequireComponent(typeof(Mover))]
+    public class PlayerRotateInput : MonoBehaviour
+    {
+        [SerializeField] private Mover mover=null;
+
+        private PlayerInputActions inputActions;
+
+        private Vector2 CorrectAxis(Vector2 axis)
+        {
+            return new Vector2(axis.y, axis.x);
+        }
+        private void PlayerRotateCanceled(InputAction.CallbackContext context)
+        {
+            Vector2 direction = context.ReadValue<Vector2>();
+            mover.rotateAxis = CorrectAxis(direction);
+        }
+
+        private void PlayerRotateStarted(InputAction.CallbackContext context)
+        {
+            Vector2 direction = context.ReadValue<Vector2>();
+            mover.rotateAxis = CorrectAxis(direction);
+        }
+
+        private void Awake()
+        {
+            inputActions = new PlayerInputActions();
+            inputActions.Player.MoveButtons.performed += PlayerRotateStarted;
+            inputActions.Player.MoveButtons.canceled += PlayerRotateCanceled;
+        }
+
+        private void OnEnable()
+        {
+            inputActions.Enable();
+        }
+        private void OnDisable()
+        {
+            inputActions.Disable();
+        }
+    }
+}
